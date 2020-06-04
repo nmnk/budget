129c6cc71c56c3a650cfbff793ce9af2f4d4d467
Daniël van Drunen <daniel@pixely.me>
2020-06-04 22:28:07 +0200

    Create RecurringRepository

---
+++ app/Repositories/RecurringRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Helper;
+use App\Models\Recurring;
+
+class RecurringRepository
+{
+    public function getValidationRules(): array
+    {
+        return [
+            'day' => ['required',
+                'regex:/\b(0?[1-9]|[12][0-9]|3[01])\b/',
+            ],
+            'end' => 'nullable|date|date_format:Y-m-d',
+            'tag' => 'nullable|exists:tags,id', // TODO CHECK IF TAG BELONGS TO USER
+            'description' => 'required|max:255',
+            'amount' => 'required|regex:/^\d*(\.\d{2})?$/'
+        ];
+    }
+
+    public function create(int $spaceId, int $day, ?string $endDate, int $tagId, string $description, string $amount): Recurring
+    {
+        return Recurring::create([
+            'space_id' => $spaceId,
+            'type' => 'monthly',
+            'day' => $day,
+            'starts_on' => date('Y-m-d'),
+            'ends_on' => $endDate,
+            'tag_id' => $tagId,
+            'description' => $description,
+            'amount' => Helper::rawNumberToInteger($amount)
+        ]);
+    }
+}
